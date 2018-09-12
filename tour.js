7397f8f96b25eee876d6cbb32bad5333238a9529
Lance Hamilton <lance.hamilton24@gmail.com>
2018-09-12 17:20:25 -0500

    created js for tour schedule

--- tour.js
+++ tour.js
@@ -1,8 +1,9 @@
 let tourSchedule = {
     schedule: [
-      {date: "September 12th", time: "7:00 pm" , venue: "Bridgestone Arena" , city: "Nashville, TN", tickets: 'www.ticketmaster.com'}  
-    ]
-}
+      {date: "September 12th", venue: "Bridgestone Arena", city: "Nashville, TN"},
+      {date: "September 12th", venue: "Bridgestone Arena", city: "Nashville, TN"}   
+    ],
+};
 
 const printToDom = (stringToPrint, divId) => {
     const selectedDiv = document.getElementById(divId);
@@ -12,14 +13,21 @@ const printToDom = (stringToPrint, divId) => {
 
 const tourStringBuilder = () => {
     let newString = '';
-    for(let i=0; i<tourSchedule.lenght; i++){
-        newString += `div class="tour">`
-        newString += `<h4>${tourSchedule.shedule[i].date}</h4>`
-        newString += `<h4>${tourSchedule.shedule[i].time}</h4>`
-        newString += `<h4>${tourSchedule.shedule[i].venue}</h4>`
-        newString += `<h4>${tourSchedule.shedule[i].city}</h4>`
-        newString += `<h4>${tourSchedule.shedule[i].tickets}</h4>`
+    for(let i=0; i<tourSchedule.schedule.length; i++){
+        newString += `<div id="tour">`
+        newString += `<h4>${tourSchedule.schedule[i].date}</h4>`
+        newString += `<h4>${tourSchedule.schedule[i].time}</h4>`
+        newString += `<h4>${tourSchedule.schedule[i].venue}</h4>`
+        newString += `<h4>${tourSchedule.schedule[i].city}</h4>`
         newString += `</div>`
     }
-    printToDom(newString, 'tour');
-}
+    printToDom(newString, "tour");
+};
+
+tourStringBuilder();
+
+
+
+
+
+// tourStringBuilder();
