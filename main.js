7aa66f9db974a4e99ff4d1b24c948daf4b6106e9
Lance Hamilton <lance.hamilton24@gmail.com>
2018-09-15 13:09:42 -0500

    added tour JavaScript to main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 let band_pictures= ["/img/img1.jpg", "/img/img2.jpg", "/img/img3.jpg"];
 let afraidOfFigs = {
-tour: "na",
 members: "na",
 albums: [
     {
@@ -23,13 +22,29 @@ albums: [
         title: "Alien Square",
         year: "2009"    
       },
-  ]
-}
+    ],
+tourInformation: [
+        {date: "September 12th", venue: "Bridgestone Arena", locations:"Nashville,TN", tickets: 'www.ticketmaster.com'},
+        {date: "September 13th", venue: "House of Blues", locations:"Chicago, IL", tickets: 'www.ticketmaster.com'},
+        {date: "September 14th", venue: "Wells Fargo Center", locations:"Philadelphia, PA", tickets: 'www.ticketmaster.com'},
+        {date: "September 15th", venue: "American Airlines Arena", locations:"Miami, FL", tickets: 'www.ticketmaster.com'},
+        {date: "September 16th", venue: "Smoothie King Center", locations:"New Orleans, LA", tickets: 'www.ticketmaster.com'},
+        {date: "September 17th", venue: "Ryman Auditorium", locations:"Nashville,TN", tickets: 'www.ticketmaster.com'},
+        {date: "September 18th", venue: "Toyota Center", locations:"Houston, TX", tickets: 'www.ticketmaster.com'},
+        {date: "September 19th", venue: "MGM Grand Garden Arena", locations:"Las Vegas, NV", tickets: 'www.ticketmaster.com'},
+        {date: "September 20th", venue: "Staples Center", locations:"Los Angeles, CA", tickets: 'www.ticketmaster.com'},
+        {date: "September 21st", venue: "The Forum", locations:"Inglewood, CA", tickets: 'www.ticketmaster.com'},
+        {date: "September 22nd", venue: "Gila River Arena", locations:"Glendale, AZ", tickets: 'www.ticketmaster.com'},
+        
+    ],
+};
+
 
 const printToDom = (stringToPrint, divId) => {
     const selectedDiv = document.getElementById(divId);
+    if(selectedDiv){
     selectedDiv.innerHTML = stringToPrint
-
+    }
 };
 
 const albumsStringBuilder = () => {
@@ -41,11 +56,8 @@ const albumsStringBuilder = () => {
         newString += `<h5>${afraidOfFigs.albums[i].year}</h5>`;
         newString += `</div>`;    
     }
-    printToDom(newString, 'albumCardA');
-    printToDom(newString, 'albumCardB');
-    
+    printToDom(newString, 'albumContainerMain');
 };
-
 
 
 const printImg = () => {
@@ -54,11 +66,26 @@ const printImg = () => {
     for(let i=0; i < band_pictures.length; i++){
 
         img += `<div class="img_band"> <img src="${band_pictures[i]}"></div>`;
-    };
+    }
 
     printToDom(img, "image_container")
-}
+};
+
+const tourStringBuilder = () => {
+    
+    let newString = '';
+    for(let i=0; i<afraidOfFigs.tourInformation.length; i++){
+        newString += `<div id="tour">`
+        newString += `<h3 class="concert">${afraidOfFigs.tourInformation[i].date}</h3>`
+        newString += `<h3 class="concert">${afraidOfFigs.tourInformation[i].venue}</h3>`
+        newString += `<h3 class="concert">${afraidOfFigs.tourInformation[i].locations}</h3>`
+        newString += `<a href="https://${afraidOfFigs.tourInformation[i].tickets}" target ="_blank">Tickets</a>`
+        newString += `</div>`
+    }
+    printToDom(newString, "tour")
+};
 
 printImg();
 albumsStringBuilder();
+tourStringBuilder();
 
