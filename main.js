103faa9fd40bf4c405c4514a647fb6015d3a9ed9
Jonathan Mohan <JonathanMohan.net@gmail.com>
2018-09-15 12:40:59 -0500

    updated HTML/CSS/JS

--- main.js
+++ main.js
@@ -1,26 +1,49 @@
 let afraidOfFigs = {
-tour: "na",
-members: "na",
 albums: [
     {
-      imageUrl: "https://orig00.deviantart.net/a457/f/2008/223/f/9/weird_square_things_by_kurachan.png",
-      title: "Squares",
+      imageUrl: "https://i.imgur.com/OFbP0lp.jpg",
+      title: "Nevermind",
       year: "2001"    
     },
     {
-        imageUrl: "http://forum.curvefever.com/sites/default/files/ideas14Mar/square%20big.png",
-        title: "More Squares",
+        imageUrl: "https://i.imgur.com/g93zMrX.jpg",
+        title: "Rumors",
         year: "2003"    
       },
     {
-        imageUrl: "https://allgiftsconsidered.com/wp-content/uploads/2013/08/weird-square-chair.jpg",
-        title: "Squares On Legs",
+        imageUrl: "https://i.imgur.com/d1QNeDz.jpg",
+        title: "Abbey Road",
         year: "2006"    
       },
     {
-        imageUrl: "https://avatars2.githubusercontent.com/u/8086224?s=88&v=4",
-        title: "Alien Square",
+        imageUrl: "https://i.imgur.com/ZdffId8.jpg",
+        title: "Blonde on Blonde",
         year: "2009"    
+      },
+    {
+        imageUrl: "https://i.imgur.com/TmPeycg.jpg",
+        title: "Appetite For Destruction",
+        year: "20011"    
+      },
+    {
+        imageUrl: "https://i.imgur.com/HAKabfZ.jpg",
+        title: "Thriller",
+        year: "2014"    
+      },
+    {
+        imageUrl: "https://i.imgur.com/5vaSoY0.jpg",
+        title: "Dark Side of the Moon",
+        year: "2016"    
+      },
+    {
+        imageUrl: "https://i.imgur.com/cSoldzo.jpg",
+        title: "Self Titled",
+        year: "2017"    
+      },
+    {
+        imageUrl: "https://i.imgur.com/McmbdoW.jpg",
+        title: "Purple Rain",
+        year: "2018"    
       },
   ]
 }
@@ -28,20 +51,22 @@ albums: [
 const printToDom = (stringToPrint, divId) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = stringToPrint
+    
 
 };
 
 const albumsStringBuilder = () => {
     let newString = '';
     for(let i = 0; i<afraidOfFigs.albums.length; i++) {
-        newString += `<div class ="image">`;
+        newString += `<div class="albumCard">`;
         newString += `<img src="${afraidOfFigs.albums[i].imageUrl}">`;
         newString += `<h2>${afraidOfFigs.albums[i].title}</h2>`;
         newString += `<h5>${afraidOfFigs.albums[i].year}</h5>`;
-        newString += `</div>`;    
+        newString += `</div>`; 
+       
     }
-    printToDom(newString, 'albumCardA');
-    printToDom(newString, 'albumCardB');
+    printToDom(newString, 'albumContainerMain');
+ 
     
 };
 
